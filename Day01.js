33085b6f686210761d8c9a0e05b217baa6e6d61f
Stuart Kemp <stuart.kemp@if.fi>
2025-12-02 15:47:56 +0200

    Day 1, Part 2 Complete

--- Day01.js
+++ Day01.js
@@ -1,3 +1,54 @@
+
+
+function parseInput(input) {
+    return input.split('\n').map(line => line.substring(1)*(line[0] === 'L' ? -1 : 1));
+}
+
+function calculateStopsAtN(movements) {
+    let stops = 0;
+    let position = 50;
+
+    for (let move of movements) {
+        position += move;
+
+        while(position < 0) position += 100;
+        while(position > 99) position -= 100;
+
+        if (position === 0) stops++;
+    }
+
+    return stops;
+}
+
+function calculatePassesN(movements) {
+    let passes = 0;
+    let position = 50;
+
+    for (let move of movements) {
+        var dir = Math.sign(move);
+        move = Math.abs(move);
+
+        while(move > 0) {
+            position += dir;
+            move--;
+
+            if(position === -1) position = 99;
+            if(position === 100) position = 0;
+
+            if(position === 0) passes++;
+        }
+    }
+
+    return passes;
+}
+
+function run(input) {
+    var parsedInput = parseInput(input);
+
+    console.log(`Stops at Zero ${calculateStopsAtN(parsedInput)} times`);
+    console.log(`Passes Zero ${calculatePassesN(parsedInput)} times`);
+}
+
 var testInput = `L68
 L30
 R48
@@ -8,27 +59,4 @@ L1
 L99
 R14
 L82`;
-
-function parseInput(input) {
-    return input.split('\n').map(line => line.substring(1)*(line[0] === 'L' ? -1 : 1));
-}
-
-function calculateStopsAtZero(movements) {
-    let stops = 0;
-    let position = 50;
-
-    for (let move of movements) {
-        position += move;
-
-        while (position < 0) position += 100;
-        while (position > 99) position -= 100;
-
-        if (position === 0) {
-            stops++;
-        }
-    }
-
-    return {stops, crosses};
-}
-
-console.log(calculateStopsAtZero(parseInput(testInput)));
+run(testInput);
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This year I'll be quick & dirty, writing in JS (not even typescript) and using work's copilot assist.
 
 Use `node {day file}` in the terminal to run a day's solution in VS Code
+
+## Day 1 ⭐⭐
+
+Of course, day 1, part 1 is simple. For part 2, I know I must be double counting somehow, but I can't figure out where. Brute force was fast enough anyway (literally clicking through and checking for 0 each move) even if it feels dirty.
